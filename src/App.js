c7d65d19ea99e2d964512cf709114dd287ead34b
Sam Autrey <34108400+swushi@users.noreply.github.com>
2021-03-18 15:16:58 -0500

    fix: updated welcome container

--- src/App.js
+++ src/App.js
@@ -3,91 +3,85 @@ import "./App.css";
 import { FaGithub } from "react-icons/fa";
 import { SiFirebase, SiReact, SiRedux, SiAmazonaws } from "react-icons/si";
 import { DiJavascript1 } from "react-icons/di";
-import { useEffect, useRef } from "react";
-
-const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+import { useEffect, useRef, useState } from "react";
 
 function App() {
-  const skillsRef = useRef(null);
-  useEffect(() => {
-    animateIcons();
-  }, []);
+  const [innerHeight, setInnerHeight] = useState(window.innerHeight);
 
-  const animateIcons = async () => {
-    const children = skillsRef.current.children;
-    for (let child of children) {
-      child.classList.add("showing");
-      await delay(75);
-    }
-  };
+  useEffect(() => {
+    window.addEventListener("resize", (evt) => {
+      setInnerHeight(window.innerHeight);
+    });
+  }, []);
 
   return (
     <div className="App">
-      <aside className="aside aside1" />
-      <main className="container">
-        <div className="welcome" style={{ height: window.innerHeight }}>
-          <a href="#" className="gh-icon">
-            <FaGithub size={30} />
-          </a>
-          <div className="name-wrapper">
-            <img
-              src="https://source.unsplash.com/random/50x50"
-              alt="alt"
-              className="avatar"
-            />
-            <div className="name">Sam Autrey</div>
-          </div>
-          <div className="intro-wrapper">
-            <div className="intro-title">Mobile Developer</div>
-            <div className="intro-bio">
-              I'm a full-stack developer with a passion for intuitive design,
-              world changing technologies, and open-source development.
-            </div>
-            <div className="welcome-buttons-wrapper">
-              <a href="#" className="contact">
-                Contact
-              </a>
-              <a href="#" className="resume">
-                See Resume
-              </a>
-            </div>
-          </div>
-          <div className="skills" ref={skillsRef}>
-            <SiReact size={50} className="skill" />
-            <SiFirebase size={50} className="skill" />
-            <DiJavascript1 size={65} className="skill" />
-            <SiRedux size={50} className="skill" />
-            <SiAmazonaws size={65} className="skill" />
+      <header style={{ height: innerHeight }}>
+        <a href="#">
+          <FaGithub size={30} />
+        </a>
+        <div className="name">
+          <img
+            src="https://source.unsplash.com/random/50x50"
+            alt="alt"
+            className="avatar"
+          />
+          <h2 class="text-center">Sam Autrey</h2>
+        </div>
+        <div className="intro">
+          <h1 class="text-center">Mobile and Web Developer</h1>
+          <p class="text-center">
+            I'm a full-stack developer with a passion for intuitive design,
+            world changing technologies, and open-source development.
+          </p>
+          <div className="buttons">
+            <a
+              href="mailto:samautrey7@gmail.com"
+              className="contact text-center"
+            >
+              Contact
+            </a>
+            <a href="#" className="resume text-center">
+              See Resume
+            </a>
           </div>
         </div>
-        <section className="offwhite">
-          <div className="bg bg-left offwhite" />
-          <div className="title primary">Demonstrated Expertise</div>
-          <div className="explain-wrapper">
-            <aside className="explain-col">
-              <div className="explain-header-wrapper">
-                <div className="explain-number">1</div>
-                <div className="explain-header">Intuitive and Modern</div>
-              </div>
-              <div className="explain-body">
-                Delivering a product that is not only intuitive, but enjoyable
-                to use is my top priority.
-              </div>
-            </aside>
-            <aside className="explain-col">
-              <div className="explain-header-wrapper">
-                <div className="explain-number">1</div>
-                <div className="explain-header two">Quickly Implemented</div>
-              </div>
-              <div className="explain-body">
-                Using Facebook’s React-Native and modern day cloud
-                infrastructure such as AWS or Google Cloud Services ensures a
-                quick, successful, and scalable product.
-              </div>
-            </aside>
-          </div>
-        </section>
-      </main>
+        <div className="skills">
+          <SiReact size={50} />
+          <SiFirebase size={50} />
+          <DiJavascript1 size={65} />
+          <SiRedux size={50} />
+          <SiAmazonaws size={65} />
+        </div>
+      </header>
+
+      <section className="offwhite">
+        <div className="bg bg-left offwhite" />
+        <div className="title primary">Demonstrated Expertise</div>
+        <div className="explain-wrapper">
+          <aside className="explain-col">
+            <div className="explain-header-wrapper">
+              <div className="explain-number">1</div>
+              <div className="explain-header">Intuitive and Modern</div>
+            </div>
+            <div className="explain-body">
+              Delivering a product that is not only intuitive, but enjoyable to
+              use is my top priority.
+            </div>
+          </aside>
+          <aside className="explain-col">
+            <div className="explain-header-wrapper">
+              <div className="explain-number">1</div>
+              <div className="explain-header two">Quickly Implemented</div>
+            </div>
+            <div className="explain-body">
+              Using Facebook’s React-Native and modern day cloud infrastructure
+              such as AWS or Google Cloud Services ensures a quick, successful,
+              and scalable product.
+            </div>
+          </aside>
+        </div>
+      </section>
       <aside className="aside aside2" />
     </div>
   );
