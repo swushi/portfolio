c3e10c5761848a58f116ceadabdbb7882d13a84a
Sam Autrey <samautrey7@gmail.com>
2021-03-21 20:02:54 -0500

    restyled header

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import { DiJavascript1 } from "react-icons/di";
 import { useEffect, useRef, useState } from "react";
 import accountable_mockup from "./accountable_mockup.png";
 import iprof_mockup from "./iprof_mockup.png";
-import avatar from "./avatar.jpg";
+import avatar from "./Portfolio_pic-removebg.png";
 
 function App() {
   const [, setInnerHeight] = useState(window.innerHeight);
@@ -37,113 +37,26 @@ function App() {
 
   return (
     <div className="App">
-      <header style={{ height: isMobile ? height : window.innerHeight }}>
-        <div class="banner" />
-        <a href="https://www.github.com/swushi">
-          <FaGithub size={30} color="#fff" />
-        </a>
-        <div className="avatar">
-          <img src={avatar} alt="alt" />
-        </div>
-        <div className="name">
-          <h2 class="text-center">Sam Autrey</h2>
-        </div>
-        <div className="intro">
-          <h1 class="text-center">Mobile and Web Developer</h1>
-          <p class="text-center">
-            I'm a full-stack developer with a passion for intuitive design,
-            world changing technologies, and open-source development.
-          </p>
-          <div className="buttons">
-            <a
-              href="mailto:samautrey7@gmail.com"
-              className="contact text-center"
-            >
-              Contact
-            </a>
-            <a href="#" className="resume text-center">
-              See Resume
-            </a>
+      <header style={{ height: window.innerHeight }}>
+        <div className="circle circle1"></div>
+        <div className="circle circle2"></div>
+        <div className="avatar"><img src={avatar} alt="Sam" /></div>
+        <navbar>
+          <h3>Skills</h3>
+          <h3>Projects</h3>
+          <h3>Resume</h3>
+        </navbar>
+        <div className="content">
+          <div className="top">
+            <h2>Hi, I'm Sam Autrey</h2>
           </div>
-        </div>
-        <div className="skills">
-          <SiReact size={50} />
-          <SiFirebase size={50} />
-          <DiJavascript1 size={65} />
-          <SiRedux size={50} />
-          <SiAmazonaws size={65} />
+          <div className="bottom">
+            <h1>Mobile and Web Developer</h1>
+            <p>Self-taught, full-stack developer with a passion for fluid design, world changing technologies, and open-source development with great experience with web and mobile apps.</p>
+            <a href="mailto:samautrey7@gmail.com" className="hello">SAY HELLO</a>
+          </div>
         </div>
       </header>
-
-      <section className="bg-offwhite">
-        <div>
-          <h2>Demonstrated Expertise</h2>
-          <div className="split">
-            <aside>
-              <div>
-                <span>1</span>
-                <h3>Intuitive and Modern</h3>
-              </div>
-              <p>
-                Delivering a product that is not only intuitive, but enjoyable
-                to use is my top priority.
-              </p>
-            </aside>
-
-            <aside>
-              <div>
-                <span>2</span>
-                <h3>Quickly Implemented</h3>
-              </div>
-              <p>
-                Using Facebook’s React-Native and modern day cloud
-                infrastructure such as AWS or Google Cloud Services ensures a
-                quick, successful, and scalable product.
-              </p>
-            </aside>
-          </div>
-          <img src={accountable_mockup} alt="a pic"></img>
-          <img src={iprof_mockup} alt="a pic"></img>
-        </div>
-      </section>
-
-      <section>
-        <div>
-          <h2>Popular in the Community</h2>
-          <div className="split">
-            <aside>
-              <div>
-                <span>1</span>
-                <h3>Open Source Projects</h3>
-              </div>
-              <p>
-                Popular Input package built with Software Mansion’s library
-                Reanimated V2, streamlining efficiency, averaging ~600 weekly
-                downloads on NPM.
-              </p>
-            </aside>
-
-            <aside>
-              <div>
-                <span>2</span>
-                <h3>Giving Back</h3>
-              </div>
-              <p>
-                Consistenly engaged in community discussions regarding anything
-                React, React-Native, or Google Cloud while also teaching less
-                experienced developers.
-              </p>
-            </aside>
-          </div>
-          <img src={accountable_mockup} alt="a pic"></img>
-          <img src={iprof_mockup} alt="a pic"></img>
-        </div>
-      </section>
-      <footer>
-        <SiLinkedin size={50} />
-        <SiGithub size={50} />
-        <SiNpm size={50} />
-      </footer>
     </div>
   );
 }
